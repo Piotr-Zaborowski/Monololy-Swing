a4e0211caaf434279edc9558d77e4905b845a91d
Piotr-Zaborowski <pzabor007@gmail.com>
2020-05-13 14:57:11 +0200

    Small fix.

--- src/com/company/GameLogic.java
+++ src/com/company/GameLogic.java
@@ -6,7 +6,7 @@ public class GameLogic {
 
     static int price[]=new int[40];
     static int owner[]=new int[40];
-    static int partsarr[][]=new int[39][7];
+    static int partsarr[][]=new int[40][7];
 
     public GameLogic()
     {
